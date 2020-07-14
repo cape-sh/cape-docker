3766436bad8aaa2620e12a8073220bd52b4df4ba
Chakradhar Rao <chakradhar.jonagam@biqmind.com>
2020-07-14 01:12:11 -0700

    use ip instead of dns name for container 2 container communication

--- hacks/mac.sh
+++ hacks/mac.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+
+export c1=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' cape-docker_cluster1_1)
+export c2=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' cape-docker_cluster2_1)
+
 sed -i.bak \
   -e 's/    certificate-authority-data.*/    insecure-skip-tls-verify: true/g' \
-  -e 's/127.0.0.1/192.168.65.2/g' \
+  -e "s/127.0.0.1/$c1/g" \
 kubeconfigs/cluster1.yaml
 
 sed -i.bak \
   -e 's/    certificate-authority-data.*/    insecure-skip-tls-verify: true/g' \
-  -e 's/127.0.0.1/192.168.65.2/g' \
+  -e "s/127.0.0.1/$c2/g" \
 kubeconfigs/cluster2.yaml
+
